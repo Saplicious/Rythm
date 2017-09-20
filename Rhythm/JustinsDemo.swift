4cf6b245fc5eaadf0c5cb7e812f2b271f3aa0701
PolyWorm <poliworm@gmail.com>
2017-09-19 17:26:55 -0700

    ..

--- Rhythm/JustinsDemo.swift
+++ Rhythm/JustinsDemo.swift
@@ -22,6 +22,7 @@ class JustinsDemo: SKScene {
     //var shadowMaster = SKNode()
     
     var arrayOfNumbers: [Int] = [-1]
+    
     var currentNumberPosition = 0
     var currentTilePosition = 0
     //position per second
@@ -34,17 +35,79 @@ class JustinsDemo: SKScene {
     var score = 0
     
     var layer = 1000000
-    //speed of blocks and player moving downwards
     
+    var buttonsMaster = SKNode()
+    
+    //speed of blocks and player moving downwards
+    override func didMove(to view: SKView) {
+        
+        let swipeLeft = UISwipeGestureRecognizer(target: self, action: #selector(self.respondToSwipeGesture))
+        swipeLeft.direction = UISwipeGestureRecognizerDirection.left
+        self.view?.addGestureRecognizer(swipeLeft)
+        
+        let swipeRight = UISwipeGestureRecognizer(target: self, action: #selector(self.respondToSwipeGesture))
+        swipeRight.direction = UISwipeGestureRecognizerDirection.right
+        self.view?.addGestureRecognizer(swipeRight)
+        
+        let swipeUp = UISwipeGestureRecognizer(target: self, action: #selector(self.respondToSwipeGesture))
+        swipeUp.direction = UISwipeGestureRecognizerDirection.up
+        self.view?.addGestureRecognizer(swipeUp)
+        
+        let swipeDown = UISwipeGestureRecognizer(target: self, action: #selector(self.respondToSwipeGesture))
+        swipeDown.direction = UISwipeGestureRecognizerDirection.down
+        self.view?.addGestureRecognizer(swipeDown)
+        
+    }
     
     override func sceneDidLoad() {
         load()
-        let background = SKSpriteNode(imageNamed: "brown")
+        //add gestures
+        buttons()
+
+        
+        let background = SKSpriteNode(imageNamed: "bg")
         background.position = CGPoint(x: 0, y: 0)
         background.size = CGSize(width: self.size.width, height: self.size.height)
         background.zPosition = 0
         addChild(background)
-        self.backgroundColor = .brown
+        
+        let ring = SKSpriteNode(imageNamed: "ring")
+        ring.position = CGPoint(x: 0, y: 0)
+        ring.size = CGSize(width: self.size.width, height: self.size.height)
+        ring.zPosition = 10000000
+        addChild(ring)
+        
+        let line = SKSpriteNode(imageNamed: "line")
+        line.position = CGPoint(x: 0, y: 0)
+        line.size = CGSize(width: self.size.width, height: self.size.height)
+        line.zPosition = 10000000
+        addChild(line)
+
+    }
+    func buttons(){
+        
+        self.addChild(buttonsMaster)
+        
+        let left = SKSpriteNode(imageNamed: "left")
+        left.position = CGPoint(x: -self.size.width * 0.33, y: -self.size.height * 0.33)
+        left.zPosition = 100000100
+        let right = SKSpriteNode(imageNamed: "right")
+        right.position = CGPoint(x: self.size.width * 0.33, y: -self.size.height * 0.33)
+        right.zPosition = 100000100
+        let up = SKSpriteNode(imageNamed: "up")
+        up.position = CGPoint(x: self.size.width * 0.20, y: -self.size.height * 0.26)
+        up.zPosition = 100000100
+        let down = SKSpriteNode(imageNamed: "down")
+        down.position = CGPoint(x: -self.size.width * 0.20, y: -self.size.height * 0.40)
+        down.zPosition = 100000100
+        
+        buttonsMaster.addChild(left)
+        buttonsMaster.addChild(right)
+        buttonsMaster.addChild(up)
+        buttonsMaster.addChild(down)
+        
+        
+        
     }
     
     //generate numbers 0,1
@@ -80,6 +143,7 @@ class JustinsDemo: SKScene {
                 // tile goes down
                 spawnTile()
             }
+            
             count += 1
         }
         
@@ -90,12 +154,13 @@ class JustinsDemo: SKScene {
         
         //load the scoreNode
         scoreNode.text = String(score)
-        scoreNode.color = UIColor.white
-        scoreNode.position = CGPoint(x:300 ,y: 600)
+        scoreNode.color = UIColor.black
+        scoreNode.position = CGPoint(x:-250 ,y: 600)
+        scoreNode.zPosition = 1000000000
         
         player = SKSpriteNode(imageNamed: "player")
-        player.size = CGSize(width: 30, height: 37.5)
-        player.position = CGPoint(x: 0 ,y: -150)
+        player.size = CGSize(width: 81, height: 138)
+        player.position = CGPoint(x: 0 ,y: -110)
         
         self.addChild(scoreNode)
         self.addChild(player)
@@ -104,7 +169,32 @@ class JustinsDemo: SKScene {
     }
     
     //move
-    func move(location: CGPoint) {
+    func respondToSwipeGesture(gesture: UIGestureRecognizer) {
+        if let swipeGesture = gesture as? UISwipeGestureRecognizer {
+            switch swipeGesture.direction {
+            case UISwipeGestureRecognizerDirection.right:
+                print("Swiped right")
+                move(location: 0)
+            case UISwipeGestureRecognizerDirection.down:
+                print("Swiped down")
+                move(location: 3)
+            case UISwipeGestureRecognizerDirection.left:
+                print("Swiped left")
+                move(location: 1)
+            case UISwipeGestureRecognizerDirection.up:
+                print("Swiped up")
+                move(location: 2)
+            default:
+                print("Swiped up")
+                break
+            }
+        }
+    }
+    
+    
+    
+    
+    func move(location: Int) {
         
         
         
@@ -133,19 +223,37 @@ class JustinsDemo: SKScene {
     
         }
     
-        if (location.x <= 0) {
+        if location == 0 {
             //move left
-            //print("moved left")
+            print("moved left")
             if arrayOfNumbers[currentNumberPosition] == 0 {
                 success()
             } else {
                 lose()
             }
             
-        } else {
+        } else if location == 1{
             //move right
-            //print("moved right")
+            print("moved right")
             if arrayOfNumbers[currentNumberPosition] == 1 {
+                success()
+            } else {
+                lose()
+            }
+            
+        } else if location == 2{
+            //move up
+            print("moved up")
+            if arrayOfNumbers[currentNumberPosition] == 2 {
+                success()
+            } else {
+                lose()
+            }
+            
+        } else if location == 3{
+            //move down
+            print("moved down")
+            if arrayOfNumbers[currentNumberPosition] == 3 {
                 success()
             } else {
                 lose()
@@ -177,9 +285,12 @@ class JustinsDemo: SKScene {
 
         //very first tile
         if tileMaster.children.count == 0 {
-            let startTile = SKSpriteNode(color: UIColor.blue, size: CGSize(width: tileSize, height: tileSize2))
-            startTile.position = CGPoint(x: 0 ,y: -200)
-            tileMaster.addChild(startTile)
+            let tile2 = SKSpriteNode(imageNamed: "block")
+            tile2.size = CGSize(width: tileSize, height: tileSize2)
+            tile2.position = CGPoint(x: 0 ,y: -200)
+            tile2.zPosition = CGFloat(layer)
+            layer = layer - 2
+            tileMaster.addChild(tile2)
             return
         }
         
@@ -187,6 +298,7 @@ class JustinsDemo: SKScene {
         
         let tile2 = SKSpriteNode(imageNamed: "block")
         tile2.size = CGSize(width: tileSize, height: tileSize2)
+        
         
         
         //check to see if tile should be spawned to the left or right
@@ -227,24 +339,53 @@ class JustinsDemo: SKScene {
         //update score
         score += 1
         scoreNode.text = String(score)
-        (tileMaster.children[currentTilePosition] as! SKSpriteNode).texture = SKTexture(imageNamed: "block")
+        
+        let b1 = SKTexture(imageNamed: "block")
+        let b2 = SKTexture(imageNamed: "block1")
+        let b3 = SKTexture(imageNamed: "block2")
+        let b4 = SKTexture(imageNamed: "block3")
+        let b5 = SKTexture(imageNamed: "block4")
+        let b6 = SKTexture(imageNamed: "block5")
+        let b7 = SKTexture(imageNamed: "block6")
+        let b8 = SKTexture(imageNamed: "block7")
+        let b9 = SKTexture(imageNamed: "block8")
+        let b10 = SKTexture(imageNamed: "block9")
+        let b11 = SKTexture(imageNamed: "block_light")
+        
+
+        let animate = SKAction.sequence([
+            SKAction.animate(with: [b1, b2, b3 ,b4 ,b5, b6 ,b7 ,b8, b9, b10, b11, b11, b10, b9], timePerFrame: 0.06)
+            ])
+        (tileMaster.children[currentTilePosition] as! SKSpriteNode).run(animate)
+        (tileMaster.children[currentTilePosition] as! SKSpriteNode).color = .white
 
         
     }
     
     //pressed wrong side
     func lose() {
-        //print("you lose")
-        (tileMaster.children[currentTilePosition] as! SKSpriteNode).color = .red
+        print("you lose")
+        let b1 = SKTexture(imageNamed: "block")
+        let b2 = SKTexture(imageNamed: "dblock1")
+        let b3 = SKTexture(imageNamed: "dblock2")
+        let b4 = SKTexture(imageNamed: "dblock3")
+        let b5 = SKTexture(imageNamed: "block_dark")
+
+        
+        
+        let animate = SKAction.sequence([
+            SKAction.animate(with: [b1, b2, b3 ,b4, b5], timePerFrame: 0.05)
+            ])
+        (tileMaster.children[currentTilePosition] as! SKSpriteNode).run(animate)
     }
     
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         
-        for t in touches {
+        //for t in touches {
             //print(""t)
-            move(location: t.location(in: self))
-        }
+            
+        //}
         
     }
     
@@ -272,7 +413,7 @@ class JustinsDemo: SKScene {
             }
             
             if currentCatcherPosition > currentNumberPosition {
-                lose()
+                
             }
         }
         
